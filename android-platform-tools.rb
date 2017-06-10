1a0890968b0b371a9a698ea98ec048fe11b809eb
caizixian <caizixian@users.noreply.github.com>
2017-06-10 22:28:23 +1000

    Create android-platform-tools.rb

--- android-platform-tools.rb
+++ android-platform-tools.rb
@@ -3,9 +3,9 @@ class AndroidPlatformTools < Formula
   homepage "https://developer.android.com/studio/releases/platform-tools.html"
   # the url is from:
   # https://dl.google.com/android/repository/repository-12.xml
-  url "https://dl.google.com/android/repository/platform-tools_r25.0.5-darwin.zip"
-  version "25.0.5"
-  sha256 "b64b3c0e8f517bd7ca673af0d8c6143d2aa58102c21318e784d7bf85a3bd238f"
+  url "https://dl.google.com/android/repository/platform-tools_r26.0.0-darwin.zip"
+  version "26.0.0"
+  sha256 "98d392cbd21ca20d643c7e1605760cc49075611e317c534096b5564053f4ac8e"
 
   bottle :unneeded
 
